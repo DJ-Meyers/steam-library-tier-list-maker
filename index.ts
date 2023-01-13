4b264943c842d1e0659ff218d3f2aa0ad38f769f
DJ Meyers <djmeyers@outlook.com>
2023-01-12 18:47:34 -0600

    Move Steam API key to keyvault

--- index.ts
+++ index.ts
@@ -3,9 +3,18 @@ import express, { Express, Request, Response } from "express";
 import path from "path";
 import cors from "cors";
 import axios from "axios";
+import { DefaultAzureCredential } from "@azure/identity";
+import { KeyVaultSecret, SecretClient } from "@azure/keyvault-secrets";
+
+const credential = new DefaultAzureCredential();
+
+const vaultName = "steam-tier-list-maker-kv"
+const url = `https://${vaultName}.vault.azure.net`;
+const client = new SecretClient(url, credential);
+
+
 
 dotenv.config();
-const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
 const app: Express = express();
 
@@ -16,8 +25,14 @@ app.get('/', (req: Request, res: Response) => {
 
 });
 
+const getSteamApiKey = async (): Promise<string> => {
+    const res: KeyVaultSecret = await client.getSecret("STEAM-API-KEY");
+    return res.value || "";
+}
+
 app.get('/api/gamesList/:steamId', async (req: Request, res: Response) => {
     const steamId = req.params.steamId;
+    const STEAM_API_KEY = await getSteamApiKey();
     const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${STEAM_API_KEY}&format=json&steamid=${steamId}&include_appinfo=true`;
     const resp = await axios.get(url);
     res.send(resp.data);
